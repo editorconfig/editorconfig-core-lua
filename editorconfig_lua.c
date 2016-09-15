eeb6af1f9b507f8e64bfcff3c1a22649c466a1a4
João Valverde <joao.valverde@tecnico.ulisboa.pt>
2016-09-15 07:05:30 +0100

    Add CMake build and editorconfig-core-test test suite
    The property table iteration order needs to be the stable
    and identical to that returned by the C library to pass the
    test suit. Therefore the parse() function now returns
    
      t = { keys; keys = values}
    
    This allows iterating in order using
    
      for _, k in ipairs(t) do print(k, t[k]) end
    
    This also allows obtaining the property count as #t.
    
    Remove Makefile and build using ISO C99.
    
    Add optional 'conf_file' argument to editorconfig_core.parse()
    
    Add CLI Lua EditorConfig script (for testing). Same interface as
    /usr/bin/editorconfig (work in progress).
    
    Fix version string to pass tests and add an optional suffix.
    
    Lastly roll back on the idea of having tokens for property
    values, at least until the test suite passes at 100%.
    
    Results:
      97% tests passed, 5 tests failed out of 179
    
      Total Test time (real) =   0.50 sec
    
      The following tests FAILED:
      	117 - indent_size_default_pre_0_9_0 (Failed)
    	157 - max_property_name (Failed)
    	158 - max_property_value (Failed)
    	159 - max_section_name_ok (Failed)
    	160 - max_section_name_ignore (Failed)

--- editorconfig_lua.c
+++ editorconfig_lua.c
@@ -31,139 +31,53 @@
 #include "lua.h"
 #include "lauxlib.h"
 
-#ifndef LEC_VERSION_STR
-#define LEC_VERSION_STR     "0.1"
+#ifndef LEC_VERSION
+#error "LEC_VERSION is not defined."
 #endif
 
-#define INDENT_STYLE_TAB    "tab@indent_style"
-#define INDENT_STYLE_SPACE  "space@indent_style"
-#define INDENT_SIZE_TAB     "tab@indent_size"
-#define END_OF_LINE_LF      "lf@end_of_line"
-#define END_OF_LINE_CRLF    "crlf@end_of_line"
-#define END_OF_LINE_CR      "cr@end_of_line"
-#define CHARSET_LATIN1      "latin1@charset"
-#define CHARSET_UTF_8       "utf-8@charset"
-#define CHARSET_UTF_8_BOM   "utf-8-bom@charset"
-#define CHARSET_UTF_16BE    "utf-16be@charset"
-#define CHARSET_UTF_16LE    "utf-16le@charset"
-#define MAXLINELEN_OFF      "off@max_line_length"
-
 #define MSGBUFSIZ   64
-#define E_OK        0
-#define E_ERROR     -1
 
 #define strequal(s1, s2)    (strcmp((s1), (s2)) == 0)
+#define strncpy0(s1, s2, n) strncpy(memset((s1), 0, n), (s2), n)
 
+#define E_OK         1
+#define E_NULL       0
+#define E_ERROR     -1
 
-static char *_strncpy0(char *dest, const char *src, size_t n) {
-    memset(dest, 0, n);
-    return strncpy(dest, src, n);
-}
+typedef int err_t;
 
-struct lec_tokens {
-    const char *name, *value;
+typedef err_t (push_ec_value_fn)(lua_State *L, const char *value);
+
+struct lec_property {
+    const char *name;
+    push_ec_value_fn *push_func;
 };
 
-static struct lec_tokens tokens[] = {
-    { "INDENT_STYLE_TAB",   INDENT_STYLE_TAB    },
-    { "INDENT_STYLE_SPACE", INDENT_STYLE_SPACE  },
-    { "INDENT_SIZE_TAB",    INDENT_SIZE_TAB     },
-    { "END_OF_LINE_LF",     END_OF_LINE_LF      },
-    { "END_OF_LINE_CRLF",   END_OF_LINE_CRLF    },
-    { "END_OF_LINE_CR",     END_OF_LINE_CR      },
-    { "CHARSET_LATIN1",     CHARSET_LATIN1      },
-    { "CHARSET_UTF_8",      CHARSET_UTF_8       },
-    { "CHARSET_UTF_8_BOM",  CHARSET_UTF_8_BOM   },
-    { "CHARSET_UTF_16BE",   CHARSET_UTF_16BE    },
-    { "CHARSET_UTF_16LE",   CHARSET_UTF_16LE    },
-    { "MAXLINELEN_OFF",     MAXLINELEN_OFF      },
-    { NULL, NULL }
-};
-
-static void add_globals(lua_State *L) {
-    int i;
-
-    for (i = 0; tokens[i].name != NULL; i++) {
-        lua_pushstring(L, tokens[i].value);
-        lua_setfield(L, -2, tokens[i].name);
-    }
+static err_t
+push_ec_string(lua_State *L, const char *value)
+{
+    lua_pushstring(L, value);
+    return E_OK;
 }
 
-static void add_version(lua_State *L) {
-    int major = 0, minor = 0, patch = 0;
-    const char *fmt;
-
-    editorconfig_get_version(&major, &minor, &patch);
-    fmt = "Lua EditorConfig v" LEC_VERSION_STR " [C Core Version %d.%d.%d]";
-    lua_pushfstring(L, fmt, major, minor, patch);
-    lua_setfield(L, -2, "_VERSION");
-}
-
-/*
- * Don't use "editorconfig_get_error_msg".
- * Error codes: https://github.com/editorconfig/editorconfig-core-c/blob/master/src/lib/editorconfig.c
- */
-static void parse_error_msg(int err_num, char *buf, size_t bufsiz) {
-    const char *msg;
-
-    if (err_num == 0)
-        msg = "no error occurred";
-    else if(err_num > 0)
-        msg = "failed to parse file";
-    else if (err_num == EDITORCONFIG_PARSE_NOT_FULL_PATH)
-        msg = "input file must be a full path name";
-    else if (err_num == EDITORCONFIG_PARSE_MEMORY_ERROR)
-        msg = "memory error";
-    else if (err_num == EDITORCONFIG_PARSE_VERSION_TOO_NEW)
-        msg = "required version is greater than the current version";
-    else
-        msg = "unknown error";
-
-    _strncpy0(buf, msg, bufsiz);
-}
-
-static void parse_error_err_file(editorconfig_handle eh, char *buf, size_t bufsiz) {
-    const char *err_file;
-
-    err_file = editorconfig_handle_get_err_file(eh);
-    if (err_file == NULL)
-        err_file = "<null>";
-
-    _strncpy0(buf, err_file, bufsiz);
-}
-
-static int parse_error(lua_State *L, editorconfig_handle eh, int err_num) {
-    char err_msg[MSGBUFSIZ], err_file[MSGBUFSIZ];
-
-    parse_error_msg(err_num, err_msg, sizeof(err_msg));
-    parse_error_err_file(eh, err_file, sizeof(err_file));
-    (void)editorconfig_handle_destroy(eh);
-
-    if (err_num == 0) {
-        return luaL_error(L, "error code is zero, probably a bug");
-    }
-
-    if (err_num < 0) {
-        return luaL_error(L, "%s", err_msg);
-    }
-
-    /* EditorConfig parsing error, 'err_num' is the line number */
-    return luaL_error(L, "'%s' at line %d: %s", err_file, err_num, err_msg);
-}
-
-static int push_ec_boolean(lua_State *L, const char *value) {
+static err_t
+push_ec_boolean(lua_State *L, const char *value)
+{
     if (strequal(value, "true")) {
         lua_pushboolean(L, 1);
-        return E_OK;
     }
-    if (strequal(value, "false")) {
+    else if (strequal(value, "false")) {
         lua_pushboolean(L, 0);
-        return E_OK;
     }
-    return E_ERROR;
+    else {
+        return E_ERROR;
+    }
+    return E_OK;
 }
 
-static int push_ec_number(lua_State *L, const char *value) {
+static err_t
+push_ec_number(lua_State *L, const char *value)
+{
     long number;
     const char *nptr = value;
     char *endptr = NULL;
@@ -181,148 +95,193 @@ static int push_ec_number(lua_State *L, const char *value) {
     return E_OK;
 }
 
-/* Push key/value on stack and call settable */
-/* Skip known keys with invalid values (according to the EC spec) */
-/* First push value, then key, then rotate */
-/* No need to pop to clean errors with rotate, keys (names) are always valid */
-static int add_ec_name_value(lua_State *L,
-                const char *name, const char *value)
+static err_t
+push_ec_indent_size(lua_State *L, const char *value)
 {
-    /* We ignore unknown values for known keys (but not unknown keys) */
-    if (strequal(name, "indent_style")) {
-        if (strequal(value, "tab")) {
-            lua_pushstring(L, INDENT_STYLE_TAB);
-        }
-        else if (strequal(value, "space")) {
-            lua_pushstring(L, INDENT_STYLE_SPACE);
-        }
-        else {
-            return 0;
+    if (strequal(value, "tab")) {
+        return push_ec_string(L, value);
+    }
+    return push_ec_number(L, value);
+}
+
+static err_t
+push_ec_max_line_length(lua_State *L, const char *value)
+{
+    if (strequal(value, "off")) {
+        return push_ec_string(L, value);
+    }
+    return push_ec_number(L, value);
+}
+
+static struct lec_property properties[] = {
+    { "indent_style",
+        push_ec_string },
+    { "indent_size",
+        push_ec_indent_size },
+    { "tab_width",
+        push_ec_number },
+    { "end_of_line",
+        push_ec_string },
+    { "charset",
+        push_ec_string },
+    { "trim_trailing_whitespace",
+        push_ec_boolean },
+    { "insert_final_newline",
+        push_ec_boolean },
+    { "max_line_length",
+        push_ec_max_line_length },
+    { NULL, NULL }
+};
+
+/* t[kn] = vn */
+/* t[n] = kn */
+static int
+add_name_value(lua_State *L, const char *name, const char *value,
+                                                    lua_Integer idx)
+{
+    push_ec_value_fn *push_func = NULL;
+
+    for (int i = 0; properties[i].name != NULL; i++) {
+        if (strequal(name, properties[i].name)) {
+            push_func = properties[i].push_func;
+            break;
         }
     }
-    else if (strequal(name, "indent_size")) {
-        if (strequal(value, "tab")) {
-            lua_pushstring(L, INDENT_SIZE_TAB);
-        }
-        else if (push_ec_number(L, value) != E_OK) {
-            return 0;
-        }
-    }
-    else if (strequal(name, "tab_width")) {
-        if (push_ec_number(L, value) != E_OK) {
-            return 0;
-        }
-    }
-    else if (strequal(name, "end_of_line")) {
-        if (strequal(value, "lf")) {
-            lua_pushstring(L, END_OF_LINE_LF);
-        }
-        else if (strequal(value, "crlf")) {
-            lua_pushstring(L, END_OF_LINE_CRLF);
-        }
-        else if (strequal(value, "cr")) {
-            lua_pushstring(L, END_OF_LINE_CR);
-        }
-        else {
-            return 0;
-        }
-    }
-    else if (strequal(name, "charset")) {
-        if (strequal(value, "latin1")) {
-            lua_pushstring(L, CHARSET_LATIN1);
-        }
-        else if (strequal(value, "utf-8")) {
-            lua_pushstring(L, CHARSET_UTF_8);
-        }
-        else if (strequal(value, "utf-8-bom")) {
-            lua_pushstring(L, CHARSET_UTF_8_BOM);
-        }
-        else if (strequal(value, "utf-16be")) {
-            lua_pushstring(L, CHARSET_UTF_16BE);
-        }
-        else if (strequal(value, "utf-16le")) {
-            lua_pushstring(L, CHARSET_UTF_16LE);
-        }
-        else {
-            return 0;
-        }
-    }
-    else if (strequal(name, "trim_trailing_whitespace")) {
-        if(push_ec_boolean(L, value) != E_OK) {
-            return 0;
-        }
-    }
-    else if (strequal(name, "insert_final_newline")) {
-        if(push_ec_boolean(L, value) != E_OK) {
-            return 0;
-        }
-    }
-    else if (strequal(name, "max_line_length")) {
-        if (strequal(value, "off")) {
-            lua_pushstring(L, MAXLINELEN_OFF);
-        }
-        else if (push_ec_number(L, value) != E_OK) {
-            return 0;
-        }
-    }
-    else {
-        /* Unknown key */
+    if (push_func == NULL) {
+        // Unknown property
         lua_pushstring(L, value);
     }
+    else if (push_func(L, value) != E_OK) {
+        return 0; // Skip
+    }
+    lua_setfield(L, -2, name);
     lua_pushstring(L, name);
-    lua_rotate(L, -2, 1);
-    lua_settable(L, -3);
-    /* One table entry inserted (for book-keeping) */
+    lua_seti(L, -2, idx);
     return 1;
 }
 
-static void create_ec_table(lua_State *L, editorconfig_handle eh) {
-    int name_value_count;
-    const char *name, *value;
-    int i, count = 0;
+static int parse_error(lua_State *L, editorconfig_handle eh, int err_num);
 
-    /* Create the EditorConfig parse table */
-    name_value_count = editorconfig_handle_get_name_value_count(eh);
-    lua_createtable(L, 0, name_value_count);
-    for (i = 0; i < name_value_count; i++) {
-        name = value = NULL;
-        editorconfig_handle_get_name_value(eh, i, &name, &value);
-        count += add_ec_name_value(L, name, value);
-    }
-    lua_pushinteger(L, count);
-}
-
-static int lec_parse(lua_State *L) {
-    const char *full_filename;
+/* One mandatory argument (full_filename) */
+/* One optional argument (conf_file_name) */
+/*
+ * Return : { keys, keys = values }
+ */
+static int
+lec_parse(lua_State *L)
+{
+    const char *full_filename, *conf_file_name;
     editorconfig_handle eh;
-    int err_num;
+    int err_num, name_value_count;
+    const char *name, *raw_value;
+    lua_Integer idx = 1;
 
     full_filename = luaL_checkstring(L, 1);
+    conf_file_name = luaL_opt(L, luaL_checkstring, 2, NULL);
 
     eh = editorconfig_handle_init();
     if (eh == NULL) {
         return luaL_error(L, "not enough memory to create handle");
+    }
+    if (conf_file_name != NULL) {
+        editorconfig_handle_set_conf_file_name(eh, conf_file_name);
     }
 
     err_num = editorconfig_parse(full_filename, eh);
     if (err_num != 0) {
         return parse_error(L, eh, err_num);
     }
-
-    create_ec_table(L, eh);
+    /* Create the EditorConfig parse table */
+    name_value_count = editorconfig_handle_get_name_value_count(eh);
+    lua_createtable(L, name_value_count, name_value_count);
+    for (int i = 0; i < name_value_count; i++) {
+        name = raw_value = NULL;
+        editorconfig_handle_get_name_value(eh, i, &name, &raw_value);
+        idx += add_name_value(L, name, raw_value, idx);
+    }
 
     (void)editorconfig_handle_destroy(eh);
-    return 2;
+    return 1;
 }
 
-static const struct luaL_Reg editorconfig_core [] = {
+/*
+ * Don't use "editorconfig_get_error_msg".
+ * Error codes: https://github.com/editorconfig/editorconfig-core-c/blob/master/src/lib/editorconfig.c
+ */
+static void
+parse_error_msg(int err_num, char *buf, size_t bufsiz)
+{
+    const char *msg;
+
+    if (err_num == 0)
+        msg = "no error occurred";
+    else if(err_num > 0)
+        msg = "failed to parse file";
+    else if (err_num == EDITORCONFIG_PARSE_NOT_FULL_PATH)
+        msg = "input file must be a full path name";
+    else if (err_num == EDITORCONFIG_PARSE_MEMORY_ERROR)
+        msg = "memory error";
+    else if (err_num == EDITORCONFIG_PARSE_VERSION_TOO_NEW)
+        msg = "required version is greater than the current version";
+    else
+        msg = "unknown error";
+
+    strncpy0(buf, msg, bufsiz);
+}
+
+static void
+parse_error_err_file(editorconfig_handle eh, char *buf, size_t bufsiz)
+{
+    const char *err_file;
+
+    err_file = editorconfig_handle_get_err_file(eh);
+    if (err_file == NULL)
+        err_file = "<null>";
+
+    strncpy0(buf, err_file, bufsiz);
+}
+
+static int
+parse_error(lua_State *L, editorconfig_handle eh, int err_num)
+{
+    char err_msg[MSGBUFSIZ], err_file[MSGBUFSIZ];
+
+    parse_error_msg(err_num, err_msg, sizeof(err_msg));
+    parse_error_err_file(eh, err_file, sizeof(err_file));
+    (void)editorconfig_handle_destroy(eh);
+
+    if (err_num == 0) {
+        return luaL_error(L, "error code is zero, probably a bug");
+    }
+    if (err_num < 0) {
+        return luaL_error(L, "%s", err_msg);
+    }
+    /* EditorConfig parsing error, 'err_num' is the line number */
+    return luaL_error(L, "'%s' at line %d: %s", err_file, err_num, err_msg);
+}
+
+static void
+add_version(lua_State *L)
+{
+    int major = 0, minor = 0, patch = 0;
+    const char *fmt;
+
+    fmt = "EditorConfig Lua Core Version %s";
+    lua_pushfstring(L, fmt, LEC_VERSION);
+    lua_setfield(L, -2, "_VERSION");
+    editorconfig_get_version(&major, &minor, &patch);
+    fmt = "EditorConfig C Core Version %d.%d.%d";
+    lua_pushfstring(L, fmt,major, minor, patch);
+    lua_setfield(L, -2, "_C_VERSION");
+}
+
+static const struct luaL_Reg editorconfig_core[] = {
     {"parse", lec_parse},
     {NULL, NULL}
 };
 
 int luaopen_editorconfig_core (lua_State *L) {
     luaL_newlib(L, editorconfig_core);
-    add_globals(L);
     add_version(L);
     return 1;
 }
